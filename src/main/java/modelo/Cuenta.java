c634f20f8d6e116cb81ab41064d7f2e19193f9b0
YimyAsprilla <yimi.mosquera@est.iudigital.edu.co>
2025-09-21 19:51:23 -0500

    Marlon

---
+++ src/main/java/modelo/Cuenta.java
@@ -0,0 +1,33 @@
+package modelo;
+
+public class Cuenta {
+    private String numeroCuenta;
+    private double saldo;
+
+    public Cuenta(String numeroCuenta, double saldo) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double monto) {
+        if (monto > 0) {
+            saldo += monto;
+        }
+    }
+
+    public boolean retirar(double monto) {
+        if (monto > 0 && monto <= saldo) {
+            saldo -= monto;
+            return true;
+        }
+        return false;
+    }
+}
